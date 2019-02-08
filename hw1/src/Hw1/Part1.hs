8002efb9ce7fc5936bdce3087d3fc27fecd058bf
Egor Makarenko <egormkn@yandex.ru>
2019-02-08 18:20:56 +0300

    Update HW1 Part 1

--- hw1/src/Hw1/Part1.hs
+++ hw1/src/Hw1/Part1.hs
@@ -6,10 +6,10 @@ order3 :: Ord a => (a, a, a) -> (a, a, a)
 order3 (x, y, z) = let [a, b, c] = sort [x, y, z] in (a, b, c)
 
 smartReplicate :: [Int] -> [Int]
-smartReplicate = concatMap (\ x -> replicate x x)
+smartReplicate = concatMap (\x -> replicate x x)
 
 contains :: Eq a => a -> [[a]] -> [[a]]
-contains x = filter $ elem x
+contains = filter . elem
 
 stringSum :: String -> Int
-stringSum string = sum $ map read $ words string
+stringSum = sum . map read . words
