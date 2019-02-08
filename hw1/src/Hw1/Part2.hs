a2a450c323c12b07280f59e52a72b4602c958b4e
Egor Makarenko <egormkn@yandex.ru>
2019-02-08 18:25:10 +0300

    Update HW1 Part 2

--- hw1/src/Hw1/Part2.hs
+++ hw1/src/Hw1/Part2.hs
@@ -1,22 +1,27 @@
 module Hw1.Part2 where
 
 removeAt :: Int -> [a] -> (Maybe a, [a])
-removeAt _ []   = (Nothing, [])
-removeAt n list@(x:xs)
-         | n < 0     = (Nothing, list)
-         | n == 0    = (Just x, xs)
-         | otherwise = (fst a, x : snd a)
-           where a = removeAt (n - 1) xs
+removeAt _ [] = (Nothing, [])
+removeAt n list@(x : xs)
+  | n < 0     = (Nothing, list)
+  | n == 0    = (Just x, xs)
+  | otherwise = (fst pair, x : snd pair) where pair = removeAt (n - 1) xs
 
 mergeSort :: Ord a => [a] -> [a]
-mergeSort [] = []
-mergeSort [x] = [x]
-mergeSort l = let merge :: Ord t => [t] -> [t] -> [t]
-                  merge a [] = a
-                  merge [] b = b
-                  merge a@(x:xs) b@(y:ys)
-                        = if x <= y
-                          then x : merge xs b
-                          else y : merge a ys
-              in merge (mergeSort $ fst m) (mergeSort $ snd m)
-                 where m = splitAt (div (length l) 2) l
+mergeSort []   = []
+mergeSort [x]  = [x]
+mergeSort list =
+  let (left, right) = split list
+  in merge (mergeSort left) (mergeSort right)
+  where
+    split :: [a] -> ([a], [a])
+    split []           = ([], [])
+    split [x]          = ([x], [])
+    split (x : y : xs) = let (evens, odds) = split xs in (x : evens, y : odds)
+
+    merge :: Ord a => [a] -> [a] -> [a]
+    merge [] r  = r
+    merge l  [] = l
+    merge a@(x : xs) b@(y : ys)
+      | x < y     = x : merge xs b
+      | otherwise = y : merge a ys
